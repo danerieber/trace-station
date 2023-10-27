797474fd296ba12bce71846f2b98fac775f691b2
Dane Rieber <danerieber@gmail.com>
2023-10-27 15:25:48 -0500

    Project setup

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  # Override nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.systems.url = "github:nix-systems/default";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    systems,
+  }:
+    flake-utils.lib.eachSystem (import systems)
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          nodePackages.pnpm
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+        ];
+      };
+    });
+}
